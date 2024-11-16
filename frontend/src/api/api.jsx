78137aee5fd50847d40cdf0b12c568730bbc278f
kritansh20ag <kritanshagarwal20@gmail.com>
2024-11-16 16:33:56 +0530

    created .env on frontend

--- frontend/src/api/api.jsx
+++ frontend/src/api/api.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
+const API_URL = import.meta.env.VITE_API_URL;
 
-const API_URL = '/api';
 
 export const getContacts = async () => await axios.get(`${API_URL}/contacts`).then((res) => res.data);
 export const addContact = async (contact) => await axios.post(`${API_URL}/contacts`, contact).then((res) => res.data);
