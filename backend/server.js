4d360d5feb7cd98f259e2c0d9effbd8d107fc895
kritansh20ag <kritanshagarwal20@gmail.com>
2024-11-16 16:41:03 +0530

    backend build errors solution

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const connectDB = require('./config/db');
-const contactRoutes = require('./routes/contactRoutes');
+const express = require("express");
+const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const connectDB = require("./config/db");
+const contactRoutes = require("./routes/contactRoutes");
 
 // Load env vars
 dotenv.config();
@@ -19,8 +19,10 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // Routes
-app.use('/api', contactRoutes);
-
+app.use("/api", contactRoutes);
+app.get("/", (req, res) => {
+  return res.send("perfect health").status(200);
+});
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
